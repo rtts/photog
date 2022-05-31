6f21294a8fbacb829d161edd29351a8f8656d67e
Jaap Joris Vens <jj@rtts.eu>
2022-05-31 15:44:08 +0200

    Update license

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Operating System :: OS Independent",
     ],
     install_requires=[

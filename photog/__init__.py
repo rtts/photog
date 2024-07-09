308671a02ba44172ad68e140c33ec7044a845ffe
Jaap Joris Vens <jj@rtts.eu>
2024-07-09 20:45:48 +0200

    Sort by seconds if sub-seconds are not available

--- photog/__init__.py
+++ photog/__init__.py
@@ -78,7 +78,7 @@ def rename_images(dir):
         basename = image.split(".", maxsplit=1)[0]
 
         # Shell out because &^%$#@! Python can't figure this out...
-        date = subprocess.check_output(["exiftool", "-SubSecDateTimeOriginal", image]).decode('utf-8').strip()
+        date = subprocess.check_output(["exiftool", "-SubSecDateTimeOriginal", "-DateTimeOriginal", os.path.join(dir, image)]).decode('utf-8').strip()
 
         photos.append(
             {
